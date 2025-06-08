5ba3a96606e5b70d6b2a24751f895110f7708974
TossherO <heyushen@sjtu.edu.cn>
2025-06-08 12:42:52 +0800

    modified:   data_preprocess/ground_removal.py

--- data_preprocess/ground_removal.py
+++ data_preprocess/ground_removal.py
@@ -5,12 +5,14 @@ import numpy as np
 import pypatchworkpp
 
 
-def ground_removal(lidar_batch, scene_save_dir):
+def ground_removal(lidar_with_sweeps, sample_idx, scene_save_dir):
     points_c = []
     num_points = []
-    for item in lidar_batch:
-        lidar_path = item['lidar_path']
-        lidar2global = item['lidar2global']
+    start = max(0, sample_idx - (args.frame_len - 1) // 2)
+    end = min(len(lidar_with_sweeps), sample_idx + (args.frame_len + 1) // 2)
+    for i in range(start, end):
+        lidar_path = lidar_with_sweeps[i]['lidar_path']
+        lidar2global = lidar_with_sweeps[i]['lidar2global']
         points = np.fromfile(lidar_path, dtype=np.float32).reshape(-1, 5)[:, :4]
         R = lidar2global[:3, :3]
         T = lidar2global[:3, 3]
@@ -19,7 +21,7 @@ def ground_removal(lidar_batch, scene_save_dir):
         num_points.append(points.shape[0])
 
     points_c = np.concatenate(points_c, axis=0, dtype=np.float32)
-    lidar2global = lidar_batch[0]['lidar2global']
+    lidar2global = lidar_with_sweeps[sample_idx]['lidar2global']
     R_inv = lidar2global[:3, :3].T
     T_inv = -R_inv @ lidar2global[:3, 3]
     points_c[:, :3] = ((R_inv @ points_c[:, :3].T).T + T_inv).astype(np.float32)
@@ -39,19 +41,20 @@ def ground_removal(lidar_batch, scene_save_dir):
     # nonground_idx = PatchworkPLUSPLUS.getNongroundIndices()
 
     idx = 0
-    for i, num in enumerate(num_points):
+    for i in range(start, end):
+        num = num_points[i - start]
         is_ground = np.zeros(num, dtype=bool)
         _ground_idx = ground_idx[(ground_idx >= idx) & (ground_idx < idx + num)] - idx
         is_ground[_ground_idx] = True
         idx += num
-        lidar_path = lidar_batch[i]['lidar_path']
+        lidar_path = lidar_with_sweeps[i]['lidar_path']
         save_path = os.path.join(scene_save_dir, os.path.basename(lidar_path))
         is_ground.tofile(save_path)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='arg parser')
-    parser.add_argument('--frame_len', type=int, default=10, help='Number of frames to process at once')
+    parser.add_argument('--frame_len', type=int, default=11, help='Number of frames to process at once')
     parser.add_argument('--info_path', type=str, default='./data/nuscenes/nuscenes_data_info.pkl', help='Path to the NuScenes data info file')
     parser.add_argument('--save_dir', type=str, default='./data/nuscenes/is_ground', help='Directory to save the ground removal results')
     return parser.parse_args()
@@ -68,21 +71,26 @@ if __name__ == '__main__':
         os.makedirs(scene_save_dir, exist_ok=True)
 
         lidar_with_sweeps = []
+        sample_idx_list = []
+        count = 0
         for sample in scene['samples']:
             for sweep in sample['lidar_sweep']:
                 lidar_with_sweeps.append({
+                    'timestamp': sweep['timestamp'],
                     'lidar_path': sweep['lidar_path'],
                     'lidar2global': sweep['ego2global'] @ sample['lidar2ego'],
                 })
+                count += 1
             lidar_with_sweeps.append({
-                    'lidar_path': sample['lidar_path'],
-                    'lidar2global': sample['ego2global'] @ sample['lidar2ego'],
-                })
+                'timestamp': sample['timestamp'],
+                'lidar_path': sample['lidar_path'],
+                'lidar2global': sample['ego2global'] @ sample['lidar2ego'],
+            })
+            sample_idx_list.append(count)
+            count += 1
 
-        for i in range(0, len(lidar_with_sweeps), args.frame_len):
-            end = min(i + args.frame_len, len(lidar_with_sweeps))
-            lidar_batch = lidar_with_sweeps[end-args.frame_len:end]
-            ground_removal(lidar_batch, scene_save_dir)
+        for sample_idx in sample_idx_list:
+            ground_removal(lidar_with_sweeps, sample_idx, scene_save_dir)
 
         print(f"Processing scene: {scene_name}, total frames: {len(lidar_with_sweeps)}")
 
